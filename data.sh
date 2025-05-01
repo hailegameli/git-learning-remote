af7aa3d912e10b985ad246112a825cda40e87632
root <root@ip-172-31-23-232.ec2.internal>
2025-05-01 00:05:42 +0000

    developer1 new code

--- data.sh
+++ data.sh
@@ -1,4 +1,5 @@
 nwe branch b1 created for this project
 my first commits for squash
 my second commits for squash
-my third commits for squash"
+my third commits for squash 
+rebasing this line from developer1
